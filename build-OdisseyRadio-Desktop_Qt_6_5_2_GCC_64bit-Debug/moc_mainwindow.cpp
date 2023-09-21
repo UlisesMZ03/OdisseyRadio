d3f73362b0d2ad6eb9b04b450ad3f6c0c31713f5
UlisesMZ03 <ulisesmz@estudiantec.cr>
2023-09-21 17:20:22 -0600

    agrupacion por artistas

--- build-OdisseyRadio-Desktop_Qt_6_5_2_GCC_64bit-Debug/moc_mainwindow.cpp
+++ build-OdisseyRadio-Desktop_Qt_6_5_2_GCC_64bit-Debug/moc_mainwindow.cpp
@@ -40,8 +40,9 @@ namespace {
 struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
 static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
     "MainWindow",
-    "actualizarBarraProgreso",
+    "actualizarMemoriaEnUso",
     "",
+    "actualizarBarraProgreso",
     "cambiarPosicionReproduccion",
     "value",
     "cargarCancion",
@@ -54,40 +55,43 @@ static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers:
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
-    uint offsetsAndSizes[24];
+    uint offsetsAndSizes[26];
     char stringdata0[11];
-    char stringdata1[24];
+    char stringdata1[23];
     char stringdata2[1];
-    char stringdata3[28];
-    char stringdata4[6];
-    char stringdata5[14];
-    char stringdata6[20];
+    char stringdata3[24];
+    char stringdata4[28];
+    char stringdata5[6];
+    char stringdata6[14];
     char stringdata7[20];
-    char stringdata8[26];
-    char stringdata9[13];
-    char stringdata10[12];
-    char stringdata11[15];
+    char stringdata8[20];
+    char stringdata9[26];
+    char stringdata10[13];
+    char stringdata11[12];
+    char stringdata12[15];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
 Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_stringdata_CLASSMainWindowENDCLASS = {
     {
         QT_MOC_LITERAL(0, 10),  // "MainWindow"
-        QT_MOC_LITERAL(11, 23),  // "actualizarBarraProgreso"
-        QT_MOC_LITERAL(35, 0),  // ""
-        QT_MOC_LITERAL(36, 27),  // "cambiarPosicionReproduccion"
-        QT_MOC_LITERAL(64, 5),  // "value"
-        QT_MOC_LITERAL(70, 13),  // "cargarCancion"
-        QT_MOC_LITERAL(84, 19),  // "detenerReproduccion"
-        QT_MOC_LITERAL(104, 19),  // "iniciarReproduccion"
-        QT_MOC_LITERAL(124, 25),  // "cargarCancionSeleccionada"
-        QT_MOC_LITERAL(150, 12),  // "formatTiempo"
-        QT_MOC_LITERAL(163, 11),  // "std::string"
-        QT_MOC_LITERAL(175, 14)   // "tiempoSegundos"
+        QT_MOC_LITERAL(11, 22),  // "actualizarMemoriaEnUso"
+        QT_MOC_LITERAL(34, 0),  // ""
+        QT_MOC_LITERAL(35, 23),  // "actualizarBarraProgreso"
+        QT_MOC_LITERAL(59, 27),  // "cambiarPosicionReproduccion"
+        QT_MOC_LITERAL(87, 5),  // "value"
+        QT_MOC_LITERAL(93, 13),  // "cargarCancion"
+        QT_MOC_LITERAL(107, 19),  // "detenerReproduccion"
+        QT_MOC_LITERAL(127, 19),  // "iniciarReproduccion"
+        QT_MOC_LITERAL(147, 25),  // "cargarCancionSeleccionada"
+        QT_MOC_LITERAL(173, 12),  // "formatTiempo"
+        QT_MOC_LITERAL(186, 11),  // "std::string"
+        QT_MOC_LITERAL(198, 14)   // "tiempoSegundos"
     },
     "MainWindow",
-    "actualizarBarraProgreso",
+    "actualizarMemoriaEnUso",
     "",
+    "actualizarBarraProgreso",
     "cambiarPosicionReproduccion",
     "value",
     "cargarCancion",
@@ -108,7 +112,7 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
       11,       // revision
        0,       // classname
        0,    0, // classinfo
-       7,   14, // methods
+       8,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -116,22 +120,24 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   56,    2, 0x08,    1 /* Private */,
-       3,    1,   57,    2, 0x08,    2 /* Private */,
-       5,    0,   60,    2, 0x08,    4 /* Private */,
-       6,    0,   61,    2, 0x08,    5 /* Private */,
-       7,    0,   62,    2, 0x08,    6 /* Private */,
-       8,    0,   63,    2, 0x08,    7 /* Private */,
-       9,    1,   64,    2, 0x08,    8 /* Private */,
+       1,    0,   62,    2, 0x08,    1 /* Private */,
+       3,    0,   63,    2, 0x08,    2 /* Private */,
+       4,    1,   64,    2, 0x08,    3 /* Private */,
+       6,    0,   67,    2, 0x08,    5 /* Private */,
+       7,    0,   68,    2, 0x08,    6 /* Private */,
+       8,    0,   69,    2, 0x08,    7 /* Private */,
+       9,    0,   70,    2, 0x08,    8 /* Private */,
+      10,    1,   71,    2, 0x08,    9 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
-    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    5,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    0x80000000 | 10, QMetaType::Double,   11,
+    0x80000000 | 11, QMetaType::Double,   12,
 
        0        // eod
 };
@@ -145,6 +151,8 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
     qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
         // Q_OBJECT / Q_GADGET
         QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'actualizarMemoriaEnUso'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'actualizarBarraProgreso'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'cambiarPosicionReproduccion'
@@ -171,13 +179,14 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         auto *_t = static_cast<MainWindow *>(_o);
         (void)_t;
         switch (_id) {
-        case 0: _t->actualizarBarraProgreso(); break;
-        case 1: _t->cambiarPosicionReproduccion((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
-        case 2: _t->cargarCancion(); break;
-        case 3: _t->detenerReproduccion(); break;
-        case 4: _t->iniciarReproduccion(); break;
-        case 5: _t->cargarCancionSeleccionada(); break;
-        case 6: { std::string _r = _t->formatTiempo((*reinterpret_cast< std::add_pointer_t<double>>(_a[1])));
+        case 0: _t->actualizarMemoriaEnUso(); break;
+        case 1: _t->actualizarBarraProgreso(); break;
+        case 2: _t->cambiarPosicionReproduccion((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 3: _t->cargarCancion(); break;
+        case 4: _t->detenerReproduccion(); break;
+        case 5: _t->iniciarReproduccion(); break;
+        case 6: _t->cargarCancionSeleccionada(); break;
+        case 7: { std::string _r = _t->formatTiempo((*reinterpret_cast< std::add_pointer_t<double>>(_a[1])));
             if (_a[0]) *reinterpret_cast< std::string*>(_a[0]) = std::move(_r); }  break;
         default: ;
         }
@@ -203,13 +212,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 7)
+        if (_id < 8)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 7;
+        _id -= 8;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 7)
+        if (_id < 8)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 7;
+        _id -= 8;
     }
     return _id;
 }
